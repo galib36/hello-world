7015eb55c87b7ff50d968a5f48e2fe1e12ceb652
galib36 <galib36@gmail.com>
2015-05-29 15:42:05 +0100

    Try again

--- Testing_another_branch/hello_world.cpp
+++ Testing_another_branch/hello_world.cpp
@@ -5,7 +5,8 @@ int main()
 {
 	int a=5;
 	printf("Printing one line and deleting other");
-	printf("testing another merging");p
+	printf("testing another merging");
+adfadfasfasdf
     printf("Hello World");
     return 0;
 }
